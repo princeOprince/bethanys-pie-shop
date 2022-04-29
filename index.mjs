b379ff5839469f713b0fe006986b9b93ea416e3b
Prince Odia <princeodia@gmail.com>
2022-04-29 12:20:45 +0100

    Create patch endpoint to patch data

--- index.mjs
+++ index.mjs
@@ -117,6 +117,40 @@ router.put('/:id', (req, res, next) => {
     );
 });
 
+router.patch('/:id', (req, res, next) => {
+    pieRepo.getById(
+        req.params.id,
+        data => {
+            if (data) {
+                pieRepo.update(
+                    req.body,
+                    req.params.id,
+                    data => {
+                        res.status(200).json({
+                            "status": 200,
+                            "statusText": "OK",
+                            "message": `Pie '${req.params.id}' patched`,
+                            "data": data
+                        });
+                    },
+                    err => next(err)
+                );
+            } else {
+                res.status(404).json({
+                    "status": 404,
+                    "statusText": "Not Found",
+                    "message": `The pie '${req.params.id}' could not be found.`,
+                    "error": {
+                        "code": "NOT_FOUND",
+                        "message": `The pie '${req.params.id}' could not be found.`
+                    }
+                });
+            }
+        },
+        err => next(err)
+    );
+});
+
 router.delete('/:id', (req, res, next) => {
     pieRepo.getById(
         req.params.id,
