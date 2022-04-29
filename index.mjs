1fe2c4c5aedd5513b607d943fdd721d18071964c
Prince Odia <princeodia@gmail.com>
2022-04-29 08:36:29 +0100

    Search for data on the rest api

--- index.mjs
+++ index.mjs
@@ -21,6 +21,53 @@ router.get('/', (req, res, next) => {
     );
 });
 
+router.get('/search', (req, res, next) => {
+    const searchObject = {
+        id: req.query.id,
+        name: req.query.name
+    }
+
+    pieRepo.search(
+        searchObject, 
+        data => {
+            res.status(200).json({
+                "status": 200,
+                "statusText": "OK",
+                "message": "All pies retrieved",
+                "data": data
+            });
+        },
+        err => next(err)
+    );
+});
+
+router.get('/:id', (req, res, next) => {
+    pieRepo.getById(
+        req.params.id,
+        data => {
+            if (data) {
+                res.status(200).json({
+                    "status": 200,
+                    "statusText": "OK",
+                    "message": "Single pie retrieved",
+                    "data": data
+                });
+            } else {
+                res.status(404).json({
+                    "status": 404,
+                    "statusText": "Not Found",
+                    "message": `The pie '${req.params.id}' could not be found.`,
+                    "error": {
+                        "code": "NOT_FOUND",
+                        "message": `The pie '${req.params.id}' could not be found.`
+                    }
+                });
+            }
+        },
+        err => next(err)
+    );
+});
+
 app.use('/api/', router);
 
 app.listen(5000, () => {
--- repos/pieRepo.mjs
+++ repos/pieRepo.mjs
@@ -10,5 +10,32 @@ export const pieRepo = {
                 resolve(JSON.parse(data));
             }
         });
+    },
+    getById: (id, resolve, reject) => {
+        fs.readFile(FILE_NAME, (err, data) => {
+            if (err) {
+                reject(err)
+            } else {
+                resolve(JSON.parse(data).find(p => p.id == id));
+            }
+        } );
+    },
+    search: (searchObject, resolve, reject) => {
+        fs.readFile(FILE_NAME, (err, data) => {
+            if (err) {
+                reject(err)
+            } else {
+                let pies = JSON.parse(data);
+                if (searchObject) {
+                    pies = pies.filter(
+                        p => (searchObject.id ? p.id == searchObject.id : true) &&
+                            (searchObject.name ? p.name.toLowerCase().indexOf(
+                                searchObject.name.toLowerCase()
+                            ) >= 0 : true)
+                    );
+                }
+                resolve(pies);
+            }
+        } )
     }
 };
