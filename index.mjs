56d40bb94d7e9d5b0ed89aca1d07d1ae75005fb2
Prince Odia <princeodia@gmail.com>
2022-04-29 04:29:42 +0100

    Return an array of data from rest api

--- index.mjs
+++ index.mjs
@@ -4,9 +4,14 @@ import debug from "debug";
 const log = debug('bethany:rest-server');
 
 const router = express.Router();
+let pies = [
+    { "id": 1, "name": "Apple"},
+    { "id": 2, "name": "Cherry"},
+    { "id": 3, "name": "Peach"}
+]
 
 router.get('/', (req, res, next) => {
-    res.send('');
+    res.send(pies);
 });
 
 app.use('/api/', router);
